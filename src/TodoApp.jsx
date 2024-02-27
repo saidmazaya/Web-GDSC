afa949bc3f6616911e6abf4e3bf59c431050d4ae
Said Muhammad Mazaya <saidmuhammad572@gmail.com>
2024-02-27 19:09:35 +0700

    Add react-icons library and update TodoApp component

--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { FaEdit, FaTrash } from "react-icons/fa";
 
 function TodoApp() {
 	function getInitialActivities() {
@@ -106,67 +107,80 @@ function TodoApp() {
 			  });
 
 	return (
-		<div className="bg-gradient-to-br from-blue-400 via-blue-500 to-blue-600 min-h-screen flex items-center justify-center">
-			<div className="bg-white w-full max-w-xl rounded shadow-lg p-8">
+		<div className="bg-gradient-to-br from-blue-400 via-blue-500 to-blue-600 min-h-screen flex justify-center items-center">
+			<div className="bg-white w-full max-w-3xl rounded shadow-lg p-8">
 				<h1 className="text-3xl font-semibold text-center mb-6">To-Do List</h1>
-				<div className="flex items-center border-b border-gray-300 py-2">
+				<div className="flex items-center border-b border-gray-300 py-2 mb-4">
 					<input type="text" value={inputValue} onChange={handleInputChange} onKeyPress={handleKeyPress} className="flex-1 px-3 py-2 mr-2 rounded border border-gray-300 focus:outline-none" placeholder="Enter your task" />
 					<button onClick={addTodo} className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 focus:outline-none">
 						Add
 					</button>
 				</div>
-				<div className="flex justify-center mt-4">
-					<button onClick={() => handleFilter("all")} className="mr-2 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 focus:outline-none">
-						All
-					</button>
-					<button onClick={() => handleFilter("completed")} className="mr-2 px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700 focus:outline-none">
-						Completed
-					</button>
-					<button onClick={() => handleFilter("uncompleted")} className="px-4 py-2 bg-red-600 text-white rounded hover:bg-red-700 focus:outline-none">
-						Uncompleted
-					</button>
+				<div className="flex justify-between items-center mb-4">
+					<div className="flex space-x-4">
+						<button onClick={() => handleFilter("all")} className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 focus:outline-none">
+							All
+						</button>
+						<button onClick={() => handleFilter("completed")} className="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700 focus:outline-none">
+							Completed
+						</button>
+						<button onClick={() => handleFilter("uncompleted")} className="px-4 py-2 bg-red-600 text-white rounded hover:bg-red-700 focus:outline-none">
+							Uncompleted
+						</button>
+					</div>
+					<div>
+						<select onChange={(e) => handleSortBy(e.target.value)} className="px-4 py-2 bg-yellow-600 text-white rounded hover:bg-yellow-700 focus:outline-none">
+							<option value="">Sort by</option>
+							<option value="createdAt">Date</option>
+							<option value="text">Text</option>
+						</select>
+					</div>
 				</div>
-				<div className="flex justify-center mt-4">
-					<button onClick={() => handleSortBy("createdAt")} className="mr-2 px-4 py-2 bg-yellow-600 text-white rounded hover:bg-yellow-700 focus:outline-none">
-						Sort by Date
-					</button>
-					<button onClick={() => handleSortBy("text")} className="px-4 py-2 bg-purple-600 text-white rounded hover:bg-purple-700 focus:outline-none">
-						Sort by Text
-					</button>
-				</div>
-				<ul className="mt-6">
-					{filteredTodos.map((todo) => (
-						<li key={todo.id} className="flex items-center justify-between mb-2">
-							<input
-								type="checkbox"
-								checked={todo.completed} // Tentukan apakah tugas sudah selesai atau belum
-								onChange={() => toggleTodo(todo.id)} // Ubah status selesai saat checkbox diubah
-								className="mr-2 form-checkbox h-5 w-5 text-blue-600 transition duration-150 ease-in-out transform hover:scale-110 focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
-							/>
-							{editId === todo.id ? (
-								<input
-									type="text"
-									value={editId === todo.id ? editInputValue : ""}
-									onChange={(event) => setEditInputValue(event.target.value)}
-									onKeyPress={handleKeyPress}
-									className="flex-1 px-3 py-2 mr-2 rounded border border-gray-300 focus:outline-none"
-								/>
-							) : (
-								<span className={todo.completed ? "line-through" : ""}>{todo.text}</span>
-							)}
-							<div>
-								{editId !== todo.id && (
-									<button onClick={() => handleEdit(todo.id)} className="mr-2 px-3 py-1 bg-indigo-600 text-white rounded hover:bg-indigo-700 focus:outline-none">
-										Edit
+				<div className="overflow-y-auto max-h-60vh">
+					<ul>
+						{filteredTodos.map((todo) => (
+							<li key={todo.id} className="flex items-center justify-between mb-2">
+								<div className="flex items-center">
+									<div onClick={() => toggleTodo(todo.id)} className={`rounded-full h-6 w-6 flex items-center justify-center border-2 cursor-pointer ${todo.completed ? "bg-blue-600 border-blue-600" : "border-gray-400"}`}>
+										{todo.completed && (
+											<svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+												<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 13l4 4L19 7" />
+											</svg>
+										)}
+									</div>
+									{/* Render input teks saat mode edit aktif */}
+									{editId === todo.id ? (
+										<input
+											type="text"
+											value={editInputValue}
+											onChange={(event) => setEditInputValue(event.target.value)}
+											onKeyPress={(event) => handleKeyPress(event)}
+											className="flex-1 px-3 ml-3 py-2 mr-2 rounded border border-gray-300 focus:outline-none"
+										/>
+									) : (
+										<span className={`ml-3 flex-1 ${todo.completed ? "text-gray-500" : ""}`}>
+											<div className={todo.completed ? "line-through" : ""}>{todo.text}</div> {/* Teks todo */}
+											<div className="text-xs text-gray-400">
+												{new Date(todo.createdAt).toLocaleTimeString()}, {new Date(todo.createdAt).toLocaleDateString()}
+											</div>
+										</span>
+									)}
+								</div>
+								<div>
+									{/* Periksa apakah mode edit aktif sebelum merender tombol edit */}
+									{editId !== todo.id && (
+										<button onClick={() => handleEdit(todo.id)} className="mr-2 px-3 py-1 bg-indigo-600 text-white rounded hover:bg-indigo-700 focus:outline-none">
+											<FaEdit />
+										</button>
+									)}
+									<button onClick={() => deleteTodo(todo.id)} className="px-3 py-1 bg-red-600 text-white rounded hover:bg-red-700 focus:outline-none">
+										<FaTrash />
 									</button>
-								)}
-								<button onClick={() => deleteTodo(todo.id)} className="px-3 py-1 bg-red-600 text-white rounded hover:bg-red-700 focus:outline-none">
-									Delete
-								</button>
-							</div>
-						</li>
-					))}
-				</ul>
+								</div>
+							</li>
+						))}
+					</ul>
+				</div>
 			</div>
 		</div>
 	);
